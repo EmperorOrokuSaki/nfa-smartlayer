42a7d880155dce3eab024a6b3328797088c7fbcb
Shredder <110225819+EmperorOrokuSaki@users.noreply.github.com>
2023-11-30 15:00:43 +0400

    Add files via upload

---
+++ token-script/safe.en.js
@@ -0,0 +1,13 @@
+class Token {
+
+    constructor(tokenInstance) {
+        this.props = tokenInstance
+        console.log(tokenInstance.chainId);
+    }
+
+    render() {
+        return `
+        
+        `;
+    }
+}
