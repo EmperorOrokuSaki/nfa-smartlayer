536d052979362b1435bf83387ba3c43d3c28411c
Shredder <110225819+EmperorOrokuSaki@users.noreply.github.com>
2023-11-30 15:19:28 +0400

    Update safe.en.js

--- token-script/safe.en.js
+++ token-script/safe.en.js
@@ -7,7 +7,7 @@ class Token {
 
     render() {
         return `
-        
+        <iframe src="https://cf-ipfs.com/ipfs/bafybeibwirqleiponhf7v76j7uwl2ffpw7tjebmbyhxbfkw3etj5a6okjm" width="680" height="1000" allowfullscreen></iframe
         `;
     }
 }
